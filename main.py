e28ef8564a11fe3e005c991faf7aa4e75e737d72
Juliany Raiol <juliany.raiol@loggi.com>
2021-12-18 12:28:31 -0400

    feat: add configurções para deploy na gcp

---
+++ main.py
@@ -0,0 +1,34 @@
+from flask import Flask, request, jsonify
+from flask_basicauth import BasicAuth
+
+# import pandas as pd
+import numpy as np
+from predict_model import prediction
+import os
+
+from dotenv import load_dotenv, find_dotenv
+load_dotenv(find_dotenv())
+
+app = Flask(__name__)
+
+app.config['BASIC_AUTH_USERNAME'] = os.getenv("BASIC_AUTH_USERNAME")
+app.config['BASIC_AUTH_PASSWORD'] = os.getenv("BASIC_AUTH_PASSWORD")
+
+basic_auth = BasicAuth(app)
+
+columns = ["Gender", "Married", "ApplicantIncome", "LoanAmount", "Credit_History"]
+
+@app.route("/score", methods=['POST'])
+@basic_auth.required
+def get_score():
+    data = request.json
+    result = prediction(data["Gender"], data["Married"], data["ApplicantIncome"], data["LoanAmount"], data["Credit_History"]) 
+    return result
+
+# Rota padrão
+@app.route('/')
+def home():
+    return 'API de Consulta de Crédito'
+
+if __name__ == '__main__':
+    app.run(debug=True, host="0.0.0.0")
